1ba90cdd9524946ab40ee4234be030caaf021b01
3shaan <3shan.bd@gmail.com>
2022-10-25 18:26:48 +0600

    course server add

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 app.use(cors());
+const courses = require('./Data/webCourses.json')
 
 app.get("/", (req, res) => {
   res.send("API is Running");
 });
+
+app.get('/course', (req, res) => {
+  res.send(courses)
+})
 
 
 
