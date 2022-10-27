96de491601c64823e5875bbb9d7c257341df0c3b
3shaan <3shan.bd@gmail.com>
2022-10-27 11:48:40 +0600

    blogs api run

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 app.use(cors());
-const courses = require('./Data/webCourses.json')
+const courses = require('./Data/webCourses.json');
+const blogs = require('./Data/Blogs.json');
 
 app.get("/", (req, res) => {
   res.send("API is Running");
@@ -17,6 +18,10 @@ app.get('/course/:id', (req, res) => {
   const id = req.params.id;
   const signleCourse = courses.find(course => course.id === id);
   res.send(signleCourse);
+});
+
+app.get('/blogs', (req, res) => {
+  res.send(blogs);
 })
 
 
